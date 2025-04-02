f3630cfe5730f869f2aa97bea3929884ccdc30c8
heejaykong <heejay.dev@gmail.com>
2025-04-03 01:21:19 +0900

    Feat: 전역 예외처리 관련 클래스 구현
    Ref: #1

---
+++ src/main/java/com/aoverflow/mmb/question_service/common/exception/CustomException.java
@@ -0,0 +1,11 @@
+package com.aoverflow.mmb.question_service.common.exception;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public class CustomException extends RuntimeException {
+
+  ErrorCode errorCode;
+}
