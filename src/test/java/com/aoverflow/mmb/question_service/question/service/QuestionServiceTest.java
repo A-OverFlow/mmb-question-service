7ccbb806fb5eb8b54d258a23fcba47ad7171b90d
heejaykong <heejay.dev@gmail.com>
2025-05-07 23:47:21 +0900

    Feature: "질문 목록 조회에 페이징 및 정렬 적용"
    * QuestionController.java, QuestionService.java
      - 질문 목록 조회에 페이징 적용.
      - 조건 디폴트값 설정(페이지 크기, 정렬 기준 등)
    * QuestionServiceTest.java
      - 페이징 적용에 따른 목록 조회 테스트 코드 수정.
    
    Resolves: #31
    Ref: N/A
    Related to: N/A

--- src/test/java/com/aoverflow/mmb/question_service/question/service/QuestionServiceTest.java
+++ src/test/java/com/aoverflow/mmb/question_service/question/service/QuestionServiceTest.java
@@ -11,11 +11,15 @@ import com.aoverflow.mmb.question_service.question.dto.QuestionUpdateDto;
 import com.aoverflow.mmb.question_service.question.entity.QuestionStatus;
 import jakarta.persistence.EntityNotFoundException;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.transaction.annotation.Transactional;
 
 @SpringBootTest
@@ -29,6 +33,8 @@ class QuestionServiceTest {
   private final static String QUESTION_CONTENT = "더미 질문 내용";
   private final static Long QUESTION_AUTHOR_ID = 123L;
   private final static String QUESTION_AUTHOR_NAME = "더미 질문 작성자";
+  private final static int PAGE_NUMBER = 0;
+  private final static int PAGE_SIZE = 3;
 
   @Test
   @DisplayName("질문 단건 조회")
@@ -50,7 +56,7 @@ class QuestionServiceTest {
   }
 
   @Test
-  @DisplayName("질문 전체 조회")
+  @DisplayName("질문 목록 조회")
   void getAll() {
     // given
     List<Long> createdQuestionIds = new ArrayList<>();
@@ -67,13 +73,25 @@ class QuestionServiceTest {
     }
 
     // when
-    List<QuestionDto> foundQuestionDtoList = questionService.getAll();
+    PageRequest pageRequest = PageRequest.of(
+        PAGE_NUMBER,
+        PAGE_SIZE,
+        Sort.by(Sort.Direction.DESC, "createdAt")
+    );
+    Page<QuestionDto> page = questionService.getAll(pageRequest);
 
     // then
-    assertEquals(createdQuestionIds, foundQuestionDtoList.stream()
-        .map(QuestionDto::getId)
-        .toList()
-    );
+    List<QuestionDto> content = page.getContent();    // 조회된 데이터
+    assertEquals(3, content.size());          // 조회된 데이터 수
+    assertEquals(10, page.getTotalElements()); // 전체 데이터 수
+    assertEquals(0, page.getNumber());      // 페이지 번호
+    assertEquals(4, page.getTotalPages());  // 전체 페이지 번호
+    assertTrue(page.isFirst());   // 첫번째 항목인가?
+    assertTrue(page.hasNext());   // 다음 페이지가 있는가?
+
+    // 정렬이 의도대로 잘 됐는가?
+    createdQuestionIds.sort(Comparator.reverseOrder());
+    assertEquals(createdQuestionIds.stream().skip(PAGE_NUMBER).limit(PAGE_SIZE).toList(), content.stream().map(QuestionDto::getId).toList());
   }
 
   @Test
